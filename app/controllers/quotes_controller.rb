4c2e5eab40aec5e91384d94a6ae3e7e3a083257d
Jake <jake.abrams9@gmail.com>
2017-05-20 17:09:16 +0000

    trying another time

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,5 @@
-class CreateQuotes < ActiveRecord: :Migration[5.0]
-  def change
-    create_table :quotes do |t|
-      t.string :saying
-      t.string :author
-      t.timestamps
-    end
+class QuotesController < ApplicationController
+  def index
   end
 end
+
